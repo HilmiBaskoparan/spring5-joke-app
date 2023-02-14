dfca84783f91644ef98b13215215c6f75e62b93e
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-02-14 17:56:32 +0300

    Creating a Spring Boot Project. Closes #1

--- src/main/java/hilmi/springframework/spring5jokeapp/Spring5JokeAppApplication.java
+++ src/main/java/hilmi/springframework/spring5jokeapp/Spring5JokeAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5JokeAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring5JokeAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokeAppApplication.class, args);
+    }
 
 }
